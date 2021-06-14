4c94babf411f97dbce7fec9fc0f4a5849a6dba9f
andersuw <anders@nr.no>
2021-06-14 14:05:21 +0200

    * bugfix

--- polartep/entrypoint.sh
+++ polartep/entrypoint.sh
@@ -7,4 +7,5 @@ cp scihub_credentials.txt /root/ai4artic_snow
 cp creodias_credentials.txt /root/ai4artic_snow
 git pull /root/ai4artic_snow
 python /root/ai4artic_snow/main.py $1
+cp /root/ai4artic_snow/fsc /workdir/fsc.tif
 
